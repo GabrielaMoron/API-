d2eb6338d14ad649c6c275970b89b92fa2f24c2c
bsquintana55 <bsquintana55@misena.edu.co>
2022-12-05 18:31:39 -0500

    album controlador - genero

--- src/models/genero.js
+++ src/models/genero.js
@@ -1,9 +1,12 @@
-const {Schema,model}=require("mongoose");
 
 const generoSchema = new mongoose.Schema({
     _id:Number,
     nombreGenero: String,
-    estadoGenero: String
+    estadoGenero: String,
+    album:[{
+        type:Schema.Types.ObjectId,
+        ref:'Album'
+      }]
 
   });
 

220f02a068b97aa70f0bd2fd67d3d6351031a68e
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-12-05 18:33:29 -0500

    conexión

---
+++ src/rutes/ruta_genero.js
@@ -0,0 +1,29 @@
+
+const {Router}=require("express");
+
+
+
+//rutas genero
+const rutasGenero=Router();
+const ctrGen=require("../controllers/generoC")
+
+const multer=require("multer");
+
+const carga=multer({storage:rutaStorage});
+
+
+
+//genero
+
+rutasGenero.get('/', ctrGen.obtener);
+
+rutasGenero.get('/:id', ctrGen.obtenerid);
+
+rutasGenero.post('/', ctrGen.add);
+  
+rutasGenero.put('/:id', ctrGen.edit);
+
+
+
+
+module.exports=rutasGenero;

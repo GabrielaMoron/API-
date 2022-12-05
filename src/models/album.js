71156a63717fd83098f325bd73453e906920be12
bsquintana55 <bsquintana55@misena.edu.co>
2022-12-05 18:05:37 -0500

    modelos

---
+++ src/models/album.js
@@ -0,0 +1,16 @@
+const {Schema,model}=require("mongoose");
+
+const albumSchema = new mongoose.Schema({
+    _id:Number,
+    nombreAlbum: String,
+    anioPublicacion: String,
+    estadoAlbum: String,
+
+    genero:[{
+      type:Schema.Types.ObjectId,
+      ref:'Genero'
+    }]
+    
+  });
+ 
+  module.exports=model("Album",albumSchema);

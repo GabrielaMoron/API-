b2814a63986f2898097ed85aeef17fb47cf92e0e
GabrielaMoron <104436274+GabrielaMoron@users.noreply.github.com>
2022-12-06 08:20:20 -0500

    rutas y vistas

---
+++ src/routes/ruta_cancion.js
@@ -0,0 +1,18 @@
+const {Router}=require("express");
+
+//rutas cancion
+const rutasCancion=Router();
+const ctrCan=require("../controllers/cancionC")
+
+
+
+//cancion
+rutasCancion.get('/', ctrCan.obtener);
+
+rutasCancion.get('/:id', ctrCan.obtenerid);
+
+rutasCancion.post('/', ctrCan.add);
+  
+rutasCancion.put('/:id',ctrCan.edit);
+
+module.exports=rutasCancion;
---
+++ src/routes/ruta_genero.js
@@ -0,0 +1,26 @@
+
+const {Router}=require("express");
+
+
+
+//rutas genero
+const rutasGenero=Router();
+const ctrGen=require("../controllers/generoC")
+
+
+
+
+//genero
+
+rutasGenero.get('/', ctrGen.obtener);
+
+rutasGenero.get('/:id', ctrGen.obtenerid);
+
+rutasGenero.post('/', ctrGen.add);
+  
+rutasGenero.put('/:id', ctrGen.edit);
+
+
+
+
+module.exports=rutasGenero;

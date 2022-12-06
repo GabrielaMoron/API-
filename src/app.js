62826eeba55dc71d8f0aa9ce3bd68c7b7e3983cd
bsquintana55 <bsquintana55@misena.edu.co>
2022-12-05 20:01:46 -0500

    vistas y rutas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const conexionBD = require('./conexion_bd');
 const rutasGenero = require('./routes/ruta_genero');
 const rutasCancion = require('./routes/ruta_cancion');
 const rutasAlbum = require('./routes/ruta_album');
+const rutaCards = require('./routes/card');
 
 const app = express()
 require('dotenv').config()
@@ -25,12 +26,16 @@ app.use(morgan("dev"));
 
 //public
 app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 
 //rutas
 app.use("/genero",rutasGenero);
 app.use("/cancion", rutasCancion);
 app.use("/album", rutasAlbum);
+app.use("/", rutaCards);
 
 
 module.exports=app;
